2971acc0490f4f00281363f2c3a6f7a82a777bf6
Eu-li <105184644+Eu-li@users.noreply.github.com>
2022-06-16 11:30:24 -0500

    Create conexión.php

---
+++ conexión.php
@@ -0,0 +1,16 @@
+#UPCI<?php
+<?php
+$ conexión = new mysqli ( "localhost" , "id18707182_loginupci" , "pw@_Re|hi_3>Aq-v" , "id18707182_login" );
+	
+	if ( $ conexión -> connect_errno )
+	{
+		echo  "No hay conexión: (" . $ conn -> connect_errno . ") " . $ conexión -> connect_error ;
+	}
+$ anfitrión = "localhost" ;
+$ usuario = "id18707182_loginupci" ;
+$ contrasena = "pw@_Re|hi_3>Aq-v" ;
+$ bd = "id18707182_login" ;
+
+$ conexion = mysqli_connect ( $ host , $ usuario , $ contrasena , $ bd );
+
+?>
